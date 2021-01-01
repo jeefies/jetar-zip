6038f38b209e6fa5305189949dccda4b1a804209
jeefies <jeefy163@163.com>
2021-01-01 23:19:03 +0800

    version2

---
+++ jetz/base.py
@@ -0,0 +1,28 @@
+import codecs
+from collections import deque
+
+
+__all__ = ('Base',)
+
+class Base:
+    def __init__(self):
+        self.files = deque()
+
+    def add(self, title, content):
+        self.files.append((title, content))
+        return len(content)
+
+    def addf(self, fn):
+        with codecs.open(fn, 'rb') as f:
+            con = f.read()
+        self.add(fn, con)
+
+    def get(self, title):
+        for t, c in self.files:
+            if t == title:
+                return c
+        else:
+            raise NoArchive(f'No Such Archive {title}')
+
+    def list(self):
+        return tuple(map(lambda x: x[0], self.files))
